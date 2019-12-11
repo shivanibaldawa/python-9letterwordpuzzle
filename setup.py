9e91e67c90fd9692287a8710cc44e3c934bc11bd
Shivani <shivani.baldawa@marlo.com.au>
2019-12-11 14:30:15 +1100

    deleted duplicate folders

--- setup.py
+++ setup.py
@@ -1,23 +1,21 @@
-"""
 import setuptools
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="python-helloworld",
+    name="wordpuzzle",
     version="0.1.0",
-    author="Frank Jung",
-    author_email="frank.jung@marlo.com.au",
-    description="Example hello world project",
+    author="Shivani Baldawa",
+    author_email="shivani.baldawa@marlo.com.au",
+    description="Word Puzzle Project",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url=
-    "https://gitlab.com/theMarloGroup/training/students/fjung/python-helloworld",
+    "https://gitlab.com/theMarloGroup/training/students/sbaldawa/python-9letterwordpuzzle",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: GPL 3",
         "Operating System :: OS Independent",
     ])
-"""
