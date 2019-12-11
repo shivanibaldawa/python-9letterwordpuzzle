0a9a3a53ddb46129f2c2da72625d6a70631a9acb
Frank Jung <frankhjung@linux.com>
2019-12-11 15:29:16 +1100

    fix errors

--- utils/helpers.py
+++ utils/helpers.py
@@ -7,8 +7,8 @@ Check if the word is valid
 
 def is_present(letters, dict_word):
     ''' Please add pydoc. '''
-    word = dict_word[:]
-    for i in letters:
+    word = list(dict_word[:])
+    for i in list(letters):
         if word.__contains__(i):
             word.remove(i)
             if len(word) == 0:
